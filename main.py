65ef2e09478a766c0e6f696a1c69acf0c1d3ad47
Vyacheslav Eykhe <vyacheslav.eyhe@gmail.com>
2018-11-22 13:04:50 +0900

    Добавлен абстрактный метод

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
+import random, abc
 animals_all = []
-class Animal:
+class Animal(abc.ABC):
     health = 'normal'
     sound = ''
     def __init__(self, name):
@@ -17,6 +17,7 @@ class Animal:
     def speak(self):
         print(self.sound)
 
+    @abc.abstractmethod
     def service(self, mode):
         if mode == 'eggs':
             print('У {} собраны яйца'.format(self.name))
@@ -32,28 +33,38 @@ class Animal:
 
 
 class Goose(Animal):
+    def service(self):
+        print('У {} собраны яйца'.format(self.name))
+
     satiety = random.randrange(0, 101)  # % сытости на момент создания объекта
     sound = 'GA-GA'
     weight = random.randrange(5000, 8000)
 
 class Cow(Animal):
+    def service(self):
+        print('Вы подоили {}'.format(self.name))
+
     satiety = random.randrange(0, 101)  # % сытости на момент создания объекта
     sound = 'MU-MU'
     weight = random.randrange(400000, 600000)
 
-    def service(self, mode):
-        if mode == 'milk' and self.health == 'normal':
+    def service(self):
+        if self.health == 'normal':
             print('Вы подоили {}'.format(self.name))
-        elif mode == 'milk' and self.health == 'death':
+        elif self.health == 'death':
             print('Ваша живность скончалась. Но это не повод не доиться ;)')
 
 
 class Sheep(Animal):
+    def service(self):
+        print('{} побрит'.format(self.name))
     satiety = random.randrange(0, 101)  # % сытости на момент создания объекта
     sound = 'ME-ME'
     weight = random.randrange(70000, 80000)
 
 class Chicken(Animal):
+    def service(self):
+        print('У {} собраны яйца'.format(self.name))
     satiety = random.randrange(0, 101)  # % сытости на момент создания объекта
     sound = 'KO-KO'
     weight = random.randrange(3000, 4000)
@@ -63,56 +74,58 @@ class Goat(Animal):
     sound = 'BE-BE'
     weight = random.randrange(50000, 150000)
 
-    def service(self, mode):
-        if mode == 'milk' and self.health == 'normal':
+    def service(self):
+        if self.health == 'normal':
             print('Вы подоили {}'.format(self.name))
-        elif mode == 'milk' and self.health == 'death':
+        elif self.health == 'death':
             print('Ваша живность скончалась. Но это не повод не доиться ;)')
 
 class Duck(Animal):
+    def service(self):
+        print('У {} собраны яйца'.format(self.name))
     satiety = random.randrange(0, 101)  # % сытости на момент создания объекта
     sound = 'Krya-Krya'
     weight = random.randrange(700, 1600)
 
 gees1 = Goose('Серый')
 gees1.eating()
-gees1.service('eggs')
+gees1.service()
 
 gees2 = Goose('Белый')
 gees2.eating()
-gees2.service('eggs')
+gees2.service()
 
 cow = Cow('Манька')
 cow.eating()
-cow.service('milk')
+cow.service()
 
 sheep1 = Sheep('Барашек')
 sheep1.eating()
-sheep1.service('shave')
+sheep1.service()
 
 sheep2 = Sheep('Кудрявый')
 sheep2.eating()
-sheep2.service('shave')
+sheep2.service()
 
 chicken1 = Chicken('Ко-Ко')
 chicken1.eating()
-chicken1.service('eggs')
+chicken1.service()
 
 chicken2 = Chicken('Кукареку')
 chicken2.eating()
-chicken2.service('eggs')
+chicken2.service()
 
 goat1 = Goat('Рога')
 goat1.eating()
-goat1.service('milk')
+goat1.service()
 
 goat2 = Goat('Копыта')
 goat2.eating()
-goat2.service('milk')
+goat2.service()
 
 duck = Duck('Кряква')
 duck.eating()
-duck.service('eggs')
+duck.service()
 
 total_weight = 0
 for animal in animals_all:
